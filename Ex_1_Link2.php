48d0d236973f6d825d92ddf2540e0564c4d889ff
ThanakornSooksumrann <Thanakorn.sook12@gmail.com>
2021-01-20 11:42:31 +0700

    update

--- Ex_1_Link2.php
+++ Ex_1_Link2.php
@@ -10,34 +10,32 @@
                 case("*") : mul(); break;
                 case("/") : div(); break;
             }
-            $x = 100;
-    function plus()
-    {
-        global $x, $y;
+            function plus()
+            {
+                global $x, $y;
 
-        echo "x + y = ",$x+$y;
-        echo "<br/>";
-    }
-    function sub()
-    {
-        global $x, $y;
-        echo "x - y = ",$x-$y ;
-        echo "<br/>";
-    }
-    function mul()
-    {
-        global $x, $y;
-        echo "x * y = ",$x*$y ;
-        echo "<br/>";
-    }
-    function div()
-    {
-        global $x, $y;
-        echo "x / y = ",$x/$y ;
-        echo "<br/>";
-    }
-            $_GET["$x"];
-            echo "<br/>";
+                echo "x + y = ",$x+$y;
+                echo "<br/>";
+            }
+            function sub()
+            {
+                global $x, $y;
+                echo "x - y = ",$x-$y ;
+                echo "<br/>";
+            }
+            function mul()
+            {
+                global $x, $y;
+                echo "x * y = ",$x*$y ;
+                echo "<br/>";
+            }
+            function div()
+            {
+                global $x, $y;
+                echo "x / y = ",$x/$y ;
+                echo "<br/>";
+            }
+                    echo "<br/>";
         ?>
     </body>
 </html>
