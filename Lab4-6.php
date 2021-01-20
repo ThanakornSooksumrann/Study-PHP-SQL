9c851b6f27b3a4e74d2077a0ba3dc96220eb2093
ThanakornSooksumrann <Thanakorn.sook12@gmail.com>
2021-01-20 08:52:13 +0700

    update

---
+++ Lab4-6.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "You must \"NOT\" forget me.<br />";
+        $name = "สมชาย";
+        echo "The variable \$name stores $name.<br />";
+        echo "We use backslash(\\) for escape sequence.<br />";
+    ?>
+</body>
+</html>
