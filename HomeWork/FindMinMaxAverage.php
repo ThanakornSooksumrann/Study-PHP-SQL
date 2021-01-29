80d9247e8262f0a8b1112f231d618dc4e73c03f9
ThanakornSooksumrann <Thanakorn.sook12@gmail.com>
2021-01-30 00:27:32 +0700

    update

--- HomeWork/FindMinMaxAverage.php
+++ HomeWork/FindMinMaxAverage.php
@@ -3,7 +3,13 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>HW1</title>
+        <style>
+            body {
+                margin: 80px;
+                padding: 20px;
+            }
+        </style>
     </head>
     <body>
     <center>
