8650d592661d9e489850fa3dfa2e82a45c9eb773
ThanakornSooksumrann <Thanakorn.sook12@gmail.com>
2021-01-30 00:21:03 +0700

    update

--- HomeWork/FindMinMaxAverage.php
+++ HomeWork/FindMinMaxAverage.php
@@ -6,24 +6,22 @@
         <title>Document</title>
     </head>
     <body>
+    <center>
         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
         <h1 align="center">HW1:Find Min Max Average</h1>
-            <center>
                 <label for="Num1">Num1:</label>
-                <input type="number" name="Num1" required><br><br>
+                <input type="number" name="Num1" style="width: 20%;" required><br><br>
                 <label for="Num2">Num2:</label>
-                <input type="number" name="Num2" required><br><br>
+                <input type="number" name="Num2" style="width: 20%;" required><br><br>
                 <label for="Num3">Num3:</label>
-                <input type="number" name="Num3" required><br><b>
-                <input type="radio" name="operator" value="mi" required> Minimum <br>
-                <input type="radio" name="operator" value="ma"> Maximum <br>
+                <input type="number" name="Num3" style="width: 20%;" required><br><br>
+                <input type="radio" name="operator" value="mi" required> Minimum 
+                <input type="radio" name="operator" value="ma"> Maximum
                 <input type="radio" name="operator" value="av"> Average <br><br>
                 <input name="btnSubmit" type="submit" value="Submit">
                 <button type="reset">clear</button>
-            </center>
         </form>
         <br>
-        <center>
         <?php
             $num1 = $_POST['Num1'];
             $num2 = $_POST['Num2'];
@@ -39,6 +37,6 @@
                 echo ("Average number = ".(($num1+$num2+$num3)/3));
             }
         ?>
-        </center>
+    </center>
     </body>
 </html>
