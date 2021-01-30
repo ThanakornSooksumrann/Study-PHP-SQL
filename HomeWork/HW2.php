5050b1c7716db9d424fb5c5d196c17f95dccef1b
ThanakornSooksumrann <Thanakorn.sook12@gmail.com>
2021-01-31 04:12:45 +0700

    update

--- HomeWork/HW2.php
+++ HomeWork/HW2.php
@@ -4,12 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        body {
-            margin: 80px;
-            padding: 20px;
-        }
-    </style>
 </head>
 <body>
     <center>
