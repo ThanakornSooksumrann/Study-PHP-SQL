553aed8fbff76b58dfafcef6e766657b3c669d58
ThanakornSooksumrann <Thanakorn.sook12@gmail.com>
2021-02-03 11:04:58 +0700

    update

--- Test/Testt.php
+++ Test/Testt.php
@@ -1,47 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+        body {
+            margin: 50px;
+            padding: 20px;
+        }
+        .button2:hover {
+            box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+        }
+    </style>
 </head>
 <body>
-<table width="80%" cellspacing="1" bgcolor="#CCCCCC">
-  <caption>รายชื่อนักเรียน และผลการเรียน</caption>
-  <col align="center"></col>
-  <col align="left"></col>
-  <col align="right"></col> 
-  <thead>
-  <tr>
-    <th width="20%" bgcolor="#FFFFCC">รหัสพนักงาน</th>
-    <th bgcolor="#FFCCCC">ชื่อ</th>
-    <th width="30%" bgcolor="#99CCFF">เงินเดือน</th>
-  </tr>
-  </thead>
-  <tbody>
-  <tr bgcolor="#FFFFFF">
-    <td>01</td>
-    <td>อำนาจ</td>
-    <td>30,000</td>
-  </tr>
-  <tr bgcolor="#FFFFFF">
-    <td>02</td>
-    <td>สมชาย</td>
-    <td>25,000</td>
-  </tr>
-  <tr bgcolor="#FFFFFF">
-    <td>03</td>
-    <td>วีระ</td>
-    <td >20,000</td>
-  </tr>
-  </tbody>
-  <tfoot>
-  <tr bgcolor="#EAF4FF">
-    <td>&nbsp;</td> 
-    <td align="right">รวม</td>
-    <td>75,000</td>
-  </tr>
-  </tfoot>
-</table>
+    <center>
+      <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <
+        <label>Number:</label>
+        <input type="number" name="number">
+        <input name="submit" type="submit" class="button2" value="ส่งค่า">
+      </form>
+      <?php
+        $num = $_POST['number'];
+        $z = 0;
+        for ($i = 0 ; $i < strlen($num) ; $i++) {
+          if($num[$i] != 0) {
+            ($num[$i] % 2 == 0) ? $eve += 1 : $odd += 1;
+          }
+          else{
+            $z += 1;
+          }
+        }
+        echo ("Odd:".$odd."<br>");
+        echo ("Eve:".$eve."<br>");
+        echo ("Zero:".$z."<br>");
+      ?>
+    </center>
 </body>
 </html>
