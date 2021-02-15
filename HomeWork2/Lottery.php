b2d953df65337271457064b75e94d0fde5c28ffb
ThanakornSooksumrann <Thanakorn.sook12@gmail.com>
2021-02-15 23:43:32 +0700

    update

--- HomeWork2/Lottery.php
+++ HomeWork2/Lottery.php
@@ -16,8 +16,7 @@
             <form action="Result-lottery.php" method="post">
                 <p><font color="blue" size="4"><b>โปรแกรม ตรวจ เลขท้าย 2 ตัว</b></font></p>
                 <p class="red">รางวัล เมื่อถูกเลขท้าย 2 ตัว (ต้นทุนซื้อ X 60 เท่า)</p>
-                <p class="red">เช่นถ้าซื้อ 10 บาท ถูกรางวัลรับเงิน 600 บาท</p>
-                <label class="red">เลขเสี่ยงโชค [0-99]</label>
+                <label class="red">เช่นถ้าซื้อ 10 บาท ถูกรางวัลรับเงิน 600 บาท<br>เลขเสี่ยงโชค [0-99]</label>
                 <input type="number" name="number" placeholder="เลขเสี่ยงโชค" min="0" max="99" style="width:150px;" required> 
                 <p><font color="red" size="5.5">*ซื้อได้ตั้งแต่ 10-500 เท่านั้น</font></p>
                 <label class="red"><font color="red" size="5.5">จำนวนเงินที่คุณต้องการซื้อ [10-500]</font></label>
