cf132c3c4a8d9d28e8d4742b6c2a06378548e57d
ThanakornSooksumrann <Thanakorn.sook12@gmail.com>
2021-02-24 09:17:32 +0700

    update

--- Lab9/Lab9_1.php
+++ Lab9/Lab9_1.php
@@ -9,9 +9,10 @@
 <body>
     <?php
     $weekdays = array("อาทิตย์", "จันทร์", "อังคาร", "พุธ", "พฤหัสบดี", "ศุกร์", "เสาร์");
-    echo '<center><font size="4" color="blue">มีนาคม ๒๕๖๐</font>';
-    echo '<font size="5" color="red"> ๓ </font>';
-    echo '<font size="4" color="blue">March 2017</font></center>';
+    echo '<center>',date("d-D-M-Y"),'</center>';
+    echo '<center><font size="4" color="blue">มกราคม ๒๕๖๔</font>';
+    echo '<font size="5" color="red"> ๒ </font>';
+    echo '<font size="4" color="blue">Jan 2021</font></center>';
     echo '<table border="1" align="center">';
     echo '<tr>';
     for ($n = 0; $n < 7; $n++) {
@@ -19,9 +20,61 @@
     }
     print_month();
     echo '</tr></table>';
+
+    $weekdays = array("อาทิตย์", "จันทร์", "อังคาร", "พุธ", "พฤหัสบดี", "ศุกร์", "เสาร์");
+    echo '<center><font size="4" color="blue">กุมภาพันธ์ ๒๕๖๔ </font>';
+    echo '<font size="4" color="blue">Feb 2021</font></center>';
+    echo '<table border="1" align="center">';
+    echo '<tr>';
+    for ($n = 0; $n < 7; $n++) {
+        echo '<td align="center" width="90">' . $weekdays[$n] . '</td>';
+    }
+    print_month2();
+    echo '</tr></table>';
+
+    $weekdays = array("อาทิตย์", "จันทร์", "อังคาร", "พุธ", "พฤหัสบดี", "ศุกร์", "เสาร์");
+    echo '<center><font size="4" color="blue">มีนาคม ๒๕๖๔ </font>';
+    echo '<font size="4" color="blue">March 2021</font></center>';
+    echo '<table border="1" align="center">';
+    echo '<tr>';
+    for ($n = 0; $n < 7; $n++) {
+        echo '<td align="center" width="90">' . $weekdays[$n] . '</td>';
+    }
+    print_month3();
+    echo '</tr></table>';
     function print_month()
     {
-        $day = -2;
+        $day = -4;
+        for ($row = 1; $row <= 5; $row++) {
+            echo '<tr>';
+            for ($col = 1; $col <= 7; $col++) {
+                if (($day >= 1) && ($day <= 31))
+                    echo '<td align="center">' . $day . '</td>';
+                else
+                    echo '<td align="center"> </td>';
+                $day++;
+            }
+        }
+    }
+
+    function print_month2()
+    {
+        $day = 0;
+        for ($row = 1; $row <= 5; $row++) {
+            echo '<tr>';
+            for ($col = 1; $col <= 7; $col++) {
+                if (($day >= 1) && ($day <= 28))
+                    echo '<td align="center">' . $day . '</td>';
+                else
+                    echo '<td align="center"> </td>';
+                $day++;
+            }
+        }
+    }
+
+    function print_month3()
+    {
+        $day = 0;
         for ($row = 1; $row <= 5; $row++) {
             echo '<tr>';
             for ($col = 1; $col <= 7; $col++) {
