e395bcb827f5f181a92da3c607a3902f2ef84603
ThanakornSooksumrann <Thanakorn.sook12@gmail.com>
2021-02-24 10:24:51 +0700

    update

--- Lab9/Lab9_2.php
+++ Lab9/Lab9_2.php
@@ -17,6 +17,8 @@
     <?php
 $MaxStudent = $_POST['stu'];
 $score = array();
+$score2 = array(0,0,0,0,0,0,0,0);
+$score3 = array('A', 'B+', 'B', 'C+', 'C', 'D+', 'D', 'F');
 for($n = 0 ; $n < $MaxStudent; $n++) {
 $score[$n] = rand(0, 100);
 }
@@ -29,40 +31,60 @@ for($n = 0 ; $n < $MaxStudent; $n++) {
 echo '<td align="center" width="90">' . ($n+1 ) . '</td>';
 echo '<td align="center" width="90">' . $score[$n] . '</td><td>'; 
 if ($score[$n] >= 80) {
+    $score2[0]++;
     echo "A";
 }
 elseif ($score[$n] >= 75) {
+    $score2[1]++;
     echo "B+";
 }
 elseif ($score[$n] >= 70) {
+    $score2[2]++;
     echo "B";
 }
 elseif ($score[$n] >= 65) {
+    $score2[3]++;
     echo "C+";
 }
 elseif ($score[$n] >= 60) {
+    $cscore2[4]++;
     echo "C";
 }
 elseif ($score[$n] >= 55) {
+    $score2[5]++;
     echo "D+";
 }
 elseif ($score[$n] >= 50) {
+    $score2[6]++;
     echo "D";
 }
 elseif ($score[$n] <= 49) {
+    $score2[7]++;
     echo "F";
 }
 echo '</td>';
 echo '</tr>';
 }
-echo '<tr><td colspan="2" align="center"> Average Score : ';
+echo '<tr><td colspan="3" align="center"> Average Score : ';
 echo average( $score, $MaxStudent);
 echo '</td></tr>';
 echo '</table>';
 echo '<center>';
-echo 'Max:',max($score);
+echo '<table border="1" align="center" width="23%">';
+echo '<tr>';
+echo '<td>Grade</td>';
+echo '<td>total</td>';
+echo '</tr>';
+for($i = 0;$i <= 7;$i++){
+    echo '<tr>';
+    echo '<td>',$score3[$i],'</td>';
+    echo '<td>',$score2[$i],'</td>';
+    echo '</tr>';
+}
+echo '</table>';
+echo 'Max:',max($score),'คนที่:',ma($score);
 echo '<br>';
-echo 'Min:',min($score);
+echo 'Min:',min($score),'คนที่:',mi($score);
 echo '</center>';
 }
 function average($data, $max) {
@@ -72,6 +94,22 @@ function average($data, $max) {
     }
     return( $total / $max );
 }
+
+    function ma($score){
+        for($i = 0;$i < count($score);$i++){
+            if($score[$i] == max($score)){
+                echo $i+1;
+            }
+        }
+    }
+
+    function mi($score){
+        for($i = 0;$i < count($score);$i++){
+            if($score[$i] == min($score)){
+                echo $i+1;
+            }
+        }
+    }
     ?>
 </body>
 
