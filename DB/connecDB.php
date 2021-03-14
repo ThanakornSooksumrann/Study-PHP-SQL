d76279f6d4db26389262687d46fd1b3ff5745e20
ThanakornSooksumrann <Thanakorn.sook12@gmail.com>
2021-03-15 01:27:11 +0700

    update

---
+++ DB/connecDB.php
@@ -0,0 +1,12 @@
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "12345";
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+    // Check connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+    ?>
