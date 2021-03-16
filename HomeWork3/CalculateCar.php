5629a83ae1ec5631642e649ab334195ac6d4f48b
ThanakornSooksumrann <Thanakorn.sook12@gmail.com>
2021-03-16 22:50:52 +0700

    update

---
+++ HomeWork3/CalculateCar.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="myStyle.css">
+    <?php session_start(); ?>
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    if (isset($_POST['sub'])) {
+        $_SESSION['carName'] = $_POST['car'];
+    }
+    $car = $_SESSION['carName'];
+    function DownPayment($percen, $price)
+    {
+        return ($price * $percen) / 100;
+    }
+    function  installment($price, $down)
+    {
+        return $price - $down;
+    }
+    function interestYear($installment, $year)
+    {
+        return ($installment * $year) / 100;
+    }
+    function  interestTotal($interestYear, $time)
+    {
+        return $interestYear * ($time / 12);
+    }
+    function payMonth($installment, $interestTotal, $time)
+    {
+        return ($installment + $interestTotal) / $time;
+    }
+    ?>
+    <div class="le">
+        <center>
+            <?php if ($car == "Accord") : ?>
+                <?php $price = 1799000;
+                $year = 2; ?>
+                <img src="Pic/logo-accord.png" class="logo"><br>
+                <img src="Pic/Accord.png" class="carpic">
+            <?php elseif ($car == "Civic") : ?>
+                <?php $price = 1145000;
+                $year = 2; ?>
+                <img src="Pic/logo-civic.png" class="logo"><br>
+                <img src="Pic/Civic.png" class="carpic">
+            <?php elseif ($car == "Jazz") : ?>
+                <?php $price = 754000;
+                $year = 4; ?>
+                <img src="Pic/logo-jazz.png" class="logo"><br>
+                <img src="Pic/Jazz.png" class="carpic">
+            <?php elseif ($car == "City") : ?>
+                <?php $price = 749000;
+                $year = 4; ?>
+                <img src="Pic/logo-city.png" class="logo"><br>
+                <img src="Pic/City.png" class="carpic">
+            <?php endif ?>
+        </center>
+    </div>
+    <div class="ri">
+
+        <form action="CalculateCar.php" method="POST">
+            <fieldset>
+                <legend>เลือกเงินดาวน์</legend>
+                เงินดาวน์
+                <input type="radio" name="type" onclick="typeinput(1)" required>
+                <select name="percen" id="percen" disabled>
+                    <option value="15">15%</option>
+                    <option value="20">20%</option>
+                    <option value="25">25%</option>
+                    <option value="30">30%</option>
+                </select>
+                หรือ ระบุจำนวนเงิน
+                <input type="radio" name="type" onclick="typeinput(2)"><br>
+                <input type="number" name="money" id="money" required disabled>
+            </fieldset>
+            <br>
+            <fieldset>
+                <legend>เลือกระยะเวลาการผ่อนชำระ</legend>
+                ระยะเวลา
+                <select name="ins">
+                    <option value="36">36</option>
+                    <option value="48">48</option>
+                    <option value="60">60</option>
+                    <option value="72">72</option>
+                </select>
+                งวด
+            </fieldset><br>
+            <center><input type="submit" name="cal" value="Calculate"></center><br>
+        </form>
+
+        <?php if (isset($_POST['cal'])) { ?>
+            <center>
+                <table border="1">
+                    <tr align="center">
+                        <td colspan="2">รายงานการคำนวณสินเชื่อผ่อนชำระ</td>
+                    </tr>
+                    <tr>
+                        <td>รุ่นรถยนต์:</td>
+                        <td><?php echo $car; ?></td>
+                    </tr>
+                    <tr>
+                        <td>ราคารถยนต์:</td>
+                        <td><?php echo number_format($price); ?></td>
+                    </tr>
+                    <tr>
+                        <td>(1) เงินดาวน์:</td>
+                        <td>
+                            <?php
+                            if (isset($_POST['percen'])) {
+                                $down = DownPayment($_POST['percen'], $price);
+                                echo number_format($down);
+                            } else {
+                                $down = $_POST['money'];
+                                echo number_format($down);
+                            } ?>
+                            บาท
+                            <?php if (isset($_POST['percen'])) {
+                                echo "(" . $_POST['percen'] . "%)";
+                            } ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>(2) จำนวนเงินที่ขอสินเชื่อ:</td>
+                        <td>
+                            <?php $installment = installment($price, $down);
+                            echo number_format($installment);
+                            ?> บาท
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>(3) ดอกเบี้ย ต่อปี:</td>
+                        <td>
+                            <?php
+                            $interestYear = interestYear($installment, $year);
+                            echo number_format($interestYear) . " บาท (" . $year . "%ต่อปี)";
+                            ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>(4) ดอกเบี้ย ทั้งหมด <?php echo $_POST['ins'] / 12; ?>ปี (<?php echo $_POST['ins'] ?> งวด) :</td>
+                        <td>
+                            <?php $interestTotal = interestTotal($interestYear, $_POST['ins']);
+                            echo number_format($interestTotal);
+                            ?> บาท
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>(5) จำนวนเงินที่ต้องผ่อน ต่อ 1 งวด:</td>
+                        <td><?php
+                            $payMonth = payMonth($installment, $interestTotal, $_POST['ins']);
+                            echo number_format($payMonth);
+                            ?> บาท
+                        </td>
+                    </tr>
+                    <tr align="center">
+                        <td colspan="2"><a href="index.php"><button>กลับไปหน้าแรก</button></a></td>
+                    </tr>
+                </table>
+            </center>
+        <?php } ?>
+    </div>
+
+    <script>
+        function typeinput(type) {
+            let elePercen = document.getElementById("percen");
+            let elemoney = document.getElementById("money");
+            if (type == 1) {
+                elePercen.removeAttribute("disabled");
+                elemoney.setAttribute("disabled", "");
+            } else {
+                elemoney.removeAttribute("disabled");
+                elePercen.setAttribute("disabled", "");
+            }
+        }
+    </script>
+</body>
+
+</html>
---
+++ HomeWork3/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="myStyle.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $carImg = array("Accord", "Civic", "Jazz", "City");
+    $carName = array("Accord", "Civic", "Jazz", "City");
+    $price = array("1,799,000", "1,145,000", "754,000", "749,000");
+    $vat = array("2.00", "2.00", "4.00", "4.00");
+    ?>
+    <div class="header">
+        <center><img src="pic/header.jpg" width="90%" height="300px"></center>
+    </div>
+    <center>
+        <form action="CalculateCar.php" method="POST">
+            <fieldset align="center">
+                <legend>Car Model</legend>
+                <center>
+                    เลือกรุ่นรถยนต์
+                    <select name="car">
+                        <option>Accord</option>
+                        <option>Civic</option>
+                        <option>Jazz</option>
+                        <option>City</option>
+                    </select>
+                    <input type="submit" name="sub" value="Submit">
+                    <table border="1">
+                        <tr align="center">
+                            <td>ภาพรถยนต์</td>
+                            <td>รุ่น<br>รถยนต์</td>
+                            <td>ราคารถยนต์<br>(บาท)</td>
+                            <td>อัตราดอกเบี้ย ต่อปี(ร้อยละ,%)</td>
+                        </tr>
+                        <?php for ($c = 0; $c < 4; $c++) { ?>
+                            <tr align="center">
+                                <td><?php echo '<img src="pic/', $carImg[$c], '.png" width="100px"'; ?>></td>
+                                <td><?php echo $carName[$c]; ?></td>
+                                <td><?php echo $price[$c]; ?></td>
+                                <td><?php echo $vat[$c]; ?></td>
+                            </tr>
+                        <?php } ?>
+                    </table>
+                </center>
+            </fieldset>
+        </form>
+    </center>
+</body>
+
+</html>
