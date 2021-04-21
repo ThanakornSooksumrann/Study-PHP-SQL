aef4707074530d1ee1558dbfc6bf994c017a6840
ThanakornSooksumrann <Thanakorn.sook12@gmail.com>
2021-04-21 12:11:36 +0700

    update

--- crud_oop/functions.php
+++ crud_oop/functions.php
@@ -5,6 +5,8 @@
     define('DB_NAME','CRUD_OOP');
 
     class DB_con {
+
+        //เช็คว่าเชื่อมต่อดาต้าเบสได้ไหม
         function __construct(){
             $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
             $this->dbcon = $conn;
@@ -14,22 +16,26 @@
             }
         }
 
+        //เพิ่มข้อมูล
         public function insert($firstname, $lastname, $email, $phonenumber, $address){
             $result = mysqli_query($this->dbcon, "INSERT INTO tbluser(firstname, lastname, email, phonenumber, address) 
             VALUES('$firstname', '$lastname', '$email', '$phonenumber', '$address')");
             return $result;
         }
 
+        //แสดงข้อมูลหน้ารายการ
         public function fetchdata(){
             $result = mysqli_query($this->dbcon, "SELECT * FROM tbluser");
             return $result;
         }
 
+        //เรียกข้อมูลไปแสดงตรงช่องข้อมูลแก้ไข
         public function fetchonerecord($userid) {
             $result = mysqli_query($this->dbcon, "SELECT * FROM tbluser WHERE id = '$userid'");
             return $result;
         }
 
+        //แก้ไขข้อมูล
         public function update($firstname, $lastname, $email, $phonenumber, $address, $userid){
             $result = mysqli_query($this->dbcon, "UPDATE tbluser SET
                 firstname = '$firstname',
@@ -42,6 +48,7 @@
             return $result;
         }
 
+        //ลบข้อมูล
         public function delete($userid) {
             $deleterecord = mysqli_query($this->dbcon, "DELETE FROM tbluser WHERE id = '$userid'");
             return $deleterecord;
