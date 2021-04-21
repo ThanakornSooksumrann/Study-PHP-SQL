99d37d1e9cfbc6b94b160ccb923550c8b86f5fe6
ThanakornSooksumrann <Thanakorn.sook12@gmail.com>
2021-04-21 10:45:52 +0700

    update

---
+++ crud_oop/insert.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" 
+    crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <a href="index.php" class="btn btn-primary mt-3">GO Back</a>
+        <hr>
+        <h1 class="mt-5">Insert Page</h1>
+        <hr>
+        <form action="" method="post">
+            <div class="mb-4">
+                <label for="firstname" class="form-label">First name</label>
+                <input type="text" class="form-control" name="firstname" required>
+            </div>
+            <div class="mb-4">
+                <label for="lastname" class="form-label">Last name</label>
+                <input type="text" class="form-control" name="lastname" required>
+            </div>
+            <div class="mb-4">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" name="email" required>
+            </div>
+            <div class="mb-4">
+                <label for="phonenumber" class="form-label">Phone Number</label>
+                <input type="text" class="form-control" name="phonenumber" required>
+            </div>
+            <div class="mb-3">
+                <label for="address" class="form-label">Address</label>
+                <textarea name="address" cols="30" rows="10" class="form-control" required></textarea>
+            </div>
+            <button type="submit" name="insert" class="btn btn-success">Insert</button>
+        </form>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" 
+        crossorigin="anonymous"></script>
+</body>
+</html>
